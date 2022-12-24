385dffc3f3d6fcc1c1bfcbd344d0224ac3ff245d
bludnic <victorcpla@gmail.com>
2022-12-24 20:11:15 +0000

    fix(grid-bot): OKx returns sometimes empty { orderId: '' } after order has been placed

--- packages/backend/src/grid-bot/utils/generateUniqId.ts
+++ packages/backend/src/grid-bot/utils/generateUniqId.ts
@@ -1,8 +1,8 @@
 import { customAlphabet } from 'nanoid';
 
-const alphaNumericAlphabet = '0123456789ABCDEF';
+const alphaNumericAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-const nanoid = customAlphabet(alphaNumericAlphabet, 9);
+const nanoid = customAlphabet(alphaNumericAlphabet, 32);
 
 export function generateUniqId(): string {
   return nanoid();
