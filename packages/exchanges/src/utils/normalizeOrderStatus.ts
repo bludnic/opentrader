38db1acdc9ffd29935d6b9e0ba8bc89f68386954
bludnic <victorcpla@gmail.com>
2023-08-13 13:27:06 +0100

    fix(`exchanges`): fix `normalizeOrderStatus`

--- packages/exchanges/src/utils/normalizeOrderStatus.ts
+++ packages/exchanges/src/utils/normalizeOrderStatus.ts
@@ -4,14 +4,16 @@ import { OrderStatus } from "@bifrost/types";
 export function normalizeOrderStatus(
   order: Pick<Order, "filled" | "amount" | "status">
 ): OrderStatus {
-  if (order.filled >= order.amount) {
-    return "filled";
-  } else if (order.filled < order.amount) {
-    return "partially_filled";
-  } else if (order.status === "open") {
+  if (order.status === "open") {
     return "open";
   } else if (order.status === "canceled") {
     return "canceled";
+  } else if (order.status === "closed") {
+    if (order.filled >= order.amount) {
+      return "filled";
+    } else if (order.filled < order.amount) {
+      return "partially_filled";
+    }
   }
 
   throw Error("normalizeOrderStatus: Impossible order status");
