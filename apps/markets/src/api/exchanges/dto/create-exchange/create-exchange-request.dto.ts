238c3d57d2de34223e2e84a73adb6a110b2a31d2
bludnic <victorcpla@gmail.com>
2023-09-20 04:40:19 +0100

    feat(Markets): add class-validator decorators

--- apps/markets/src/api/exchanges/dto/create-exchange/create-exchange-request.dto.ts
+++ apps/markets/src/api/exchanges/dto/create-exchange/create-exchange-request.dto.ts
@@ -1,6 +1,7 @@
 import { $Enums } from '@bifrost/markets-prisma';
 import { ExchangeCode } from '@bifrost/types';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
 import { Exchange } from 'src/core/prisma/types/exchange/exchange.type';
 
 export class CreateExchangeRequestDto
@@ -9,8 +10,10 @@ export class CreateExchangeRequestDto
   @ApiProperty({
     enum: ExchangeCode,
   })
+  @IsEnum(ExchangeCode)
   code: $Enums.ExchangeCode;
 
   @ApiProperty()
+  @IsString()
   name: string;
 }
--- apps/markets/src/api/markets/dto/create-market/create-market-request.dto.ts
+++ apps/markets/src/api/markets/dto/create-market/create-market-request.dto.ts
@@ -1,12 +1,16 @@
 import { ExchangeCode } from '@bifrost/types';
 import { ApiProperty } from '@nestjs/swagger';
 import { $Enums } from '@bifrost/markets-prisma';
+import { IsEnum, IsString } from 'class-validator';
 import { Market } from 'src/core/prisma/types';
 
 export class CreateMarketRequestDto implements Omit<Market, 'updatedAt'> {
+  @IsString()
   symbol: string;
+
   @ApiProperty({
     enum: ExchangeCode,
   })
+  @IsEnum(ExchangeCode)
   exchangeCode: $Enums.ExchangeCode;
 }
