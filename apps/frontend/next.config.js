c3f5911a483158bd6f706935067e7aecb5c1f1ad
bludnic <victorcpla@gmail.com>
2023-10-04 15:36:05 +0100

    fix(frontend): Prisma schema error workaround when using in a monorepo with Next.js

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,5 +1,9 @@
 const { z } = require("zod");
 
+// Solves: Error: Your schema.prisma could not be found
+// @docs https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 const envValidationSchema = z.object({
   NEXT_PUBLIC_BACKEND_API_URL: z.string().min(1),
   NEXT_PUBLIC_MARKETS_API_URL: z.string().min(1),
@@ -9,4 +13,11 @@ envValidationSchema.parse(process.env); // validate ENV schema
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
