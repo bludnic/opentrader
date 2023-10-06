4c5c15b10b6787900ca91acb9e2b6718848ca456
bludnic <victorcpla@gmail.com>
2023-10-06 02:26:09 +0100

    chore: remove deprecated package `@opentrader/swagger`

--- packages/swagger/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { Tag } from "../tags";
-
-export interface IEndpoint<OperationId> {
-  tagName(): Tag["name"];
-  operation(operationId: OperationId): {
-    operationId: OperationId;
-  };
-}
