7181a0a5f849f1637bc288bbeb48f26fd7e9558e
bludnic <victorcpla@gmail.com>
2023-11-08 00:39:40 +0000

    feat(grid-bot): add "Create new bot" button

--- apps/frontend/src/app/dashboard/grid-bot/list/page.tsx
+++ apps/frontend/src/app/dashboard/grid-bot/list/page.tsx
@@ -1,3 +1,5 @@
+import NextLink from "next/link";
+import Button from "@mui/joy/Button";
 import Grid from "@mui/joy/Grid";
 import React from "react";
 import { BotCard } from "src/components/grid-bot/bots-list/BotCard";
@@ -8,6 +10,16 @@ export default async function Page() {
 
   return (
     <Grid container spacing={4}>
+      <Grid xs={12}>
+        <Button
+          size="lg"
+          component={NextLink}
+          href="/dashboard/grid-bot/create"
+        >
+          Create new bot
+        </Button>
+      </Grid>
+
       <Grid xl={4} md={5} xs={12}>
         {bots.map((bot, i) => (
           <BotCard
