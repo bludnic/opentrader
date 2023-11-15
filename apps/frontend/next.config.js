e3caafbab2e1758231b2e10fb3344125a29f5e60
bludnic <victorcpla@gmail.com>
2023-11-15 00:29:24 +0000

    chore: prisma workaround no more needed (#73)

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,9 +1,5 @@
 const { z } = require("zod");
 
-// Solves: Error: Your schema.prisma could not be found
-// @docs https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
-
 const envValidationSchema = z.object({
   NEXT_PUBLIC_PROCESSOR_API_URL: z.string().min(1),
   POSTGRES_URL: z.string().min(1),
@@ -15,10 +11,6 @@ envValidationSchema.parse(process.env); // validate ENV schema
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
     // Solves: Module not found: `bufferutil` and `utf-8-validate`
     // when importing `ccxt` in a Server Component (#57)
     config.externals.push({
