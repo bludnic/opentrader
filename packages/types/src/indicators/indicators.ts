b7a50d798119764823c1e07f850946c696f56e06
bludnic <victorcpla@gmail.com>
2024-01-31 05:32:01 +0000

    feat(indicators): add more timeframes

--- packages/types/src/indicators/indicators.ts
+++ packages/types/src/indicators/indicators.ts
@@ -1,7 +1,10 @@
 import type { BarSize } from "../common";
 
 export type IndicatorName = "SMA10" | "SMA15" | "SMA30" | "RSI";
-export type IndicatorBarSize = Extract<BarSize, "1m" | "5m">;
+export type IndicatorBarSize = Extract<
+  BarSize,
+  "1m" | "5m" | "10m" | "15m" | "30m" | "1h" | "4h" | "1d"
+>;
 
 export type IndicatorsResult<I extends IndicatorName> = {
   [K in I]: number;
