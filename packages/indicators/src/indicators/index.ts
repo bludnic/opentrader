14bb84fdd90c929c63279f94682e9da090ef5ecd
bludnic <victorcpla@gmail.com>
2024-02-01 04:38:48 +0000

    feat(indicators): add SMA5

--- packages/indicators/src/indicators/index.ts
+++ packages/indicators/src/indicators/index.ts
@@ -1,20 +1,28 @@
+import type { IndicatorBarSize } from "@opentrader/types";
 import type { IndicatorsMap } from "../types/indicators";
 import { computeSMA } from "./compute";
 
+const timeframes: IndicatorBarSize[] = ["1m", "5m", "15m", "1h", "4h", "1d"];
+
 export const indicatorsMap: IndicatorsMap = {
+  SMA5: {
+    periods: 5,
+    timeframes,
+    compute: (candles) => computeSMA(candles, 5),
+  },
   SMA10: {
     periods: 10,
-    timeframes: ["1m", "5m"],
+    timeframes,
     compute: (candles) => computeSMA(candles, 10),
   },
   SMA15: {
     periods: 15,
-    timeframes: ["1m", "5m"],
+    timeframes,
     compute: (candles) => computeSMA(candles, 15),
   },
   SMA30: {
     periods: 30,
-    timeframes: ["1m", "5m"],
+    timeframes,
     compute: (candles) => computeSMA(candles, 30),
   },
   RSI: { periods: NaN, timeframes: ["1m", "5m"], compute: () => NaN },
--- packages/types/src/indicators/indicators.ts
+++ packages/types/src/indicators/indicators.ts
@@ -1,6 +1,6 @@
 import type { BarSize } from "../common";
 
-export type IndicatorName = "SMA10" | "SMA15" | "SMA30" | "RSI";
+export type IndicatorName = "SMA5" | "SMA10" | "SMA15" | "SMA30" | "RSI";
 export type IndicatorBarSize = Extract<
   BarSize,
   "1m" | "5m" | "10m" | "15m" | "30m" | "1h" | "4h" | "1d"
