50abfda51c23a64e7d082edab992bdba142a2923
bludnic <victorcpla@gmail.com>
2024-05-12 08:51:59 +0100

    refactor(processor): remove `OrderPublisher`

--- apps/processor/src/processing/order-publisher/constants.ts
+++
@@ -1,9 +0,0 @@
-/**
- * How often to run the OrderPublisher job (in seconds)
- */
-export const ORDER_PUBLISHER_INTERVAL = 5000;
-
-/**
- * Restart job timeout when there are no orders to be placed.
- */
-export const ORDER_PUBLISHER_TIMEOUT_IF_NO_ORDERS = 60000;
