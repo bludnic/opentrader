d9f2c27b4d19dde6e67251300a0a17ae9a8e9cce
bludnic <victorcpla@gmail.com>
2024-05-28 04:33:08 +0100

    fix(strategies): add schema to BotTemplate type

--- packages/bot-processor/src/types/bot/bot-template.type.ts
+++ packages/bot-processor/src/types/bot/bot-template.type.ts
@@ -1,7 +1,9 @@
+import type { ZodObject } from "zod";
 import type { TBotContext } from "./bot-context.type";
 import type { IBotConfiguration } from "./bot-configuration.interface";
 
 export interface BotTemplate<T extends IBotConfiguration> {
   (ctx: TBotContext<T>): Generator<unknown, unknown>;
   requiredHistory?: number;
+  schema: ZodObject<any, any, any>;
 }
