6f89e4548af6a08e94d2aeb4c04946a66a0809c1
bludnic <victorcpla@gmail.com>
2024-06-20 00:34:16 +0100

    chore(ESLint): unsafe call rules, error -> warn

--- packages/eslint/module.js
+++ packages/eslint/module.js
@@ -17,8 +17,8 @@ export default tseslint.config({
   rules: {
     '@typescript-eslint/no-unsafe-argument': 'error',
     '@typescript-eslint/no-unsafe-assignment': 'error',
-    '@typescript-eslint/no-unsafe-call': 'error',
-    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
     '@typescript-eslint/no-unsafe-return': 'error'
   }
 })
