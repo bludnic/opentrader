1af853e8e974bd33daae8252d305c4dfbd5ff1ed
bludnic <victorcpla@gmail.com>
2024-07-16 07:11:27 +0100

    chore(cli): prisma migrate `dev -> deploy`

--- scripts/utils/generate-package-json.mjs
+++ scripts/utils/generate-package-json.mjs
@@ -77,7 +77,7 @@ export function generatePackageJson(outDir) {
 
   // Add postinstall script
   newPackageJson.scripts.postinstall =
-    'prisma generate && DATABASE_URL="file:${HOME}/.opentrader/dev.db" prisma migrate dev --skip-generate && node scripts/postinstall.mjs';
+    'DATABASE_URL="file:${HOME}/.opentrader/dev.db" prisma migrate deploy --skip-generate && node scripts/postinstall.mjs';
 
   // Write to new package.json file
   newPackageJson.prisma = {
