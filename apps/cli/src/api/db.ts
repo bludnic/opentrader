66b6b85c325df83b277e96fcd78771e66d6d4c01
bludnic <victorcpla@gmail.com>
2024-07-16 04:28:16 +0100

    fix(cli): incorrect `prisma` path in `db migrate` command

--- apps/cli/src/api/db.ts
+++ apps/cli/src/api/db.ts
@@ -1,10 +1,17 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import { execa } from "execa";
 import { logger } from "@opentrader/logger";
 import { CommandResult } from "../types.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const ROOT_DIR = join(__dirname, "../"); // relative to ./bin
+const PRISMA_BIN = join(ROOT_DIR, "node_modules/prisma/build/index.js");
+
 export async function db(operation: string): Promise<CommandResult> {
   if (operation === "migrate") {
-    execa("npx prisma migrate dev", {
+    execa(`${PRISMA_BIN} migrate dev --skip-generate`, {
       stdio: "inherit",
       shell: true,
     });
