9fb6ba0c22c59c2a750eb142bbe9e4e7ad129f13
bludnic <victorcpla@gmail.com>
2024-07-25 23:33:23 +0100

    chore(k8s, NO_PERMISSION): mount to `/mnt/data/opentrader` instead of `/data/opentrader`

--- bin/docker-entry.sh
+++ bin/docker-entry.sh
@@ -3,10 +3,6 @@
 # This is the entry point for Docker container
 # to run the DB migrations before starting the app
 
-# Ensure the mounted volume directory exists
-mkdir -p /data/opentrader
-chmod 755 /data/opentrader
-
 # Run database migrations
 /app/node_modules/prisma/build/index.js migrate deploy --schema /app/packages/prisma/src/schema.prisma
 
