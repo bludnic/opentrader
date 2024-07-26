53bdec9d041a0e4d4c2503327ea681bed6d3e3cd
bludnic <victorcpla@gmail.com>
2024-07-26 15:59:57 +0100

    chore(Docker, k8s): create app directory path before database is created

--- bin/docker-entry.sh
+++ bin/docker-entry.sh
@@ -3,6 +3,9 @@
 # This is the entry point for Docker container
 # to run the DB migrations before starting the app
 
+# Ensure the data directory exists. This is where the SQLite database will be stored
+mkdir -p /app/data
+
 # Run database migrations
 /app/node_modules/prisma/build/index.js migrate deploy --schema /app/packages/prisma/src/schema.prisma
 
