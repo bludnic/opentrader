746682c3706b485c2ed1ddca5cbbb3114f206389
bludnic <victorcpla@gmail.com>
2024-09-05 00:43:31 +0100

    fix(OrdersHistory): order status Deleted not implemented causing runtime error

--- packages/db/src/entities/order.entity.ts
+++ packages/db/src/entities/order.entity.ts
@@ -1,13 +1,7 @@
 import { XOrderStatus, XOrderType } from "@opentrader/types";
 import type { Order as OrderModel } from "@prisma/client";
 
-type GenericOrderProps =
-  | "type"
-  | "status"
-  | "price"
-  | "filledPrice"
-  | "filledAt"
-  | "placedAt";
+type GenericOrderProps = "type" | "status" | "price" | "filledPrice" | "filledAt" | "placedAt";
 
 type OrderEntityBuilder<
   OrderType extends XOrderType,
@@ -17,45 +11,17 @@ type OrderEntityBuilder<
   type: OrderType;
   status: OrderStatus;
   price: OrderType extends "Limit" ? NonNullable<O["price"]> : null;
-  filledPrice: OrderStatus extends "Filled"
-    ? NonNullable<O["filledPrice"]>
-    : null;
+  filledPrice: OrderStatus extends "Filled" ? NonNullable<O["filledPrice"]> : null;
   filledAt: OrderStatus extends "Filled" ? NonNullable<O["filledAt"]> : null;
-  placedAt: OrderStatus extends "Placed" | "Filled" | "Canceled"
-    ? NonNullable<O["placedAt"]>
-    : null;
+  placedAt: OrderStatus extends "Placed" | "Filled" | "Canceled" ? NonNullable<O["placedAt"]> : null;
 } & Omit<O, GenericOrderProps>;
 
-export type LimitOrderIdle<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Idle",
-  T
->;
-export type LimitOrderPlaced<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Placed",
-  T
->;
-export type LimitOrderFilled<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Filled",
-  T
->;
-export type LimitOrderCanceled<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Canceled",
-  T
->;
-export type LimitOrderRevoked<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Revoked",
-  T
->;
-export type LimitOrderDeleted<T extends OrderModel> = OrderEntityBuilder<
-  "Limit",
-  "Deleted",
-  T
->;
+export type LimitOrderIdle<T extends OrderModel> = OrderEntityBuilder<"Limit", "Idle", T>;
+export type LimitOrderPlaced<T extends OrderModel> = OrderEntityBuilder<"Limit", "Placed", T>;
+export type LimitOrderFilled<T extends OrderModel> = OrderEntityBuilder<"Limit", "Filled", T>;
+export type LimitOrderCanceled<T extends OrderModel> = OrderEntityBuilder<"Limit", "Canceled", T>;
+export type LimitOrderRevoked<T extends OrderModel> = OrderEntityBuilder<"Limit", "Revoked", T>;
+export type LimitOrderDeleted<T extends OrderModel> = OrderEntityBuilder<"Limit", "Deleted", T>;
 
 export type LimitOrder<T extends OrderModel = OrderModel> =
   | LimitOrderIdle<T>
@@ -65,36 +31,12 @@ export type LimitOrder<T extends OrderModel = OrderModel> =
   | LimitOrderRevoked<T>
   | LimitOrderDeleted<T>;
 
-export type MarketOrderIdle<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Idle",
-  T
->;
-export type MarketOrderPlaced<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Placed",
-  T
->;
-export type MarketOrderFilled<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Filled",
-  T
->;
-export type MarketOrderCanceled<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Canceled",
-  T
->;
-export type MarketOrderRevoked<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Revoked",
-  T
->;
-export type MarketOrderDeleted<T extends OrderModel> = OrderEntityBuilder<
-  "Market",
-  "Deleted",
-  T
->;
+export type MarketOrderIdle<T extends OrderModel> = OrderEntityBuilder<"Market", "Idle", T>;
+export type MarketOrderPlaced<T extends OrderModel> = OrderEntityBuilder<"Market", "Placed", T>;
+export type MarketOrderFilled<T extends OrderModel> = OrderEntityBuilder<"Market", "Filled", T>;
+export type MarketOrderCanceled<T extends OrderModel> = OrderEntityBuilder<"Market", "Canceled", T>;
+export type MarketOrderRevoked<T extends OrderModel> = OrderEntityBuilder<"Market", "Revoked", T>;
+export type MarketOrderDeleted<T extends OrderModel> = OrderEntityBuilder<"Market", "Deleted", T>;
 
 export type MarketOrder<T extends OrderModel = OrderModel> =
   | MarketOrderIdle<T>
@@ -104,13 +46,11 @@ export type MarketOrder<T extends OrderModel = OrderModel> =
   | MarketOrderRevoked<T>
   | MarketOrderDeleted<T>;
 
-export type OrderEntity<T extends OrderModel = OrderModel> =
-  | LimitOrder<T>
-  | MarketOrder<T>;
+export type OrderEntity<T extends OrderModel = OrderModel> = LimitOrder<T> | MarketOrder<T>;
 
 export function toOrderEntity<T extends OrderModel>(order: T): OrderEntity<T> {
-  const { type, status, price, filledPrice, filledAt, placedAt, ...baseProps } =
-    order;
+  const { type, price, filledPrice, filledAt, placedAt, ...baseProps } = order;
+  const status = order.status as XOrderStatus;
 
   if (type === "Limit") {
     switch (status) {
@@ -159,6 +99,7 @@ export function toOrderEntity<T extends OrderModel>(order: T): OrderEntity<T> {
           placedAt: assertNonNullable(placedAt, "placedAt"),
         };
       case "Revoked":
+      case "Deleted":
         return {
           ...baseProps,
           type,
@@ -169,9 +110,8 @@ export function toOrderEntity<T extends OrderModel>(order: T): OrderEntity<T> {
           filledAt: null,
           placedAt: null,
         };
-      case "Deleted": {
-        throw new Error('Not implemented yet: "Deleted" case');
-      }
+      default:
+        throw new Error(`Not implemented yet: "${status}" case`);
     }
   } else if (type === "Market") {
     switch (status) {
@@ -220,6 +160,7 @@ export function toOrderEntity<T extends OrderModel>(order: T): OrderEntity<T> {
           placedAt: assertNonNullable(placedAt, "placedAt"),
         };
       case "Revoked":
+      case "Deleted":
         return {
           ...baseProps,
           type,
@@ -230,19 +171,15 @@ export function toOrderEntity<T extends OrderModel>(order: T): OrderEntity<T> {
           filledAt: null,
           placedAt: null,
         };
-      case "Deleted": {
-        throw new Error('Not implemented yet: "Deleted" case');
-      }
+      default:
+        throw new Error(`Not implemented yet: "${status}" case`);
     }
   }
 
   throw new Error("toOrderModel: Unexpected error");
 }
 
-function assertNonNullable<T extends number | string | Date | null>(
-  value: T,
-  name?: string,
-): NonNullable<T> {
+function assertNonNullable<T extends number | string | Date | null>(value: T, name?: string): NonNullable<T> {
   if (value === null) {
     throw new Error(`assert: ${name} is null.`);
   }
