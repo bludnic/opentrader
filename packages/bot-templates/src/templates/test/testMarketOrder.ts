8df473645565006ff73210d43febb167f27f45fc
bludnic <victorcpla@gmail.com>
2024-09-08 17:16:35 +0100

    feat(bot-templates): add `testMarketOrder` strategy

---
+++ packages/bot-templates/src/templates/test/testMarketOrder.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { buy, cancelSmartTrade, IBotConfiguration, TBotContext } from "@opentrader/bot-processor";
+import { logger } from "@opentrader/logger";
+
+export function* testMarketOrder(ctx: TBotContext<TestMarketOrderConfig, TestMarketOrderState>) {
+  const { config: bot, onStart, onStop } = ctx;
+  const { settings } = bot;
+
+  if (onStart) {
+    logger.info(bot, "[Test Market Order] Strategy started with params");
+
+    yield buy({
+      pair: bot.symbol,
+      quantity: settings.quantityToBuy,
+      orderType: "Market",
+    });
+
+    return;
+  }
+
+  if (onStop) {
+    yield cancelSmartTrade("0");
+    return;
+  }
+
+  logger.info(bot, "[Test Market Order] Strategy triggered");
+}
+
+testMarketOrder.displayName = "Test Market Order";
+testMarketOrder.hidden = true;
+testMarketOrder.schema = z.object({
+  quantityToBuy: z.number().positive().default(0.001).describe("Quantity to buy"),
+});
+testMarketOrder.timeframe = ({ timeframe }: IBotConfiguration) => timeframe;
+testMarketOrder.watchers = {
+  watchCandles: true,
+};
+testMarketOrder.runPolicy = {
+  onCandleClosed: true,
+};
+
+type TestMarketOrderState = {};
+type TestMarketOrderConfig = IBotConfiguration<z.infer<typeof testMarketOrder.schema>>;
